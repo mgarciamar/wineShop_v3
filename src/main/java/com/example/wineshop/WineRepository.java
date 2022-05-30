cd1389999a9de89975c2e9ef476fd7ca5ee0abde
Mónica García <monica88sm@gmail.com>
2022-05-30 09:44:21 +0200

    Modificaciones en WineRepository.java

--- src/main/java/com/example/wineshop/WineRepository.java
+++ src/main/java/com/example/wineshop/WineRepository.java
@@ -22,7 +22,7 @@ public interface WineRepository extends JpaRepository<Wine, Long>{
     @Query(value="SELECT * FROM wine order by rating/price desc LIMIT ?1 ", nativeQuery = true)
     Collection<Wine> findBangWines(Integer top);
 
-    @Query(value="select * from wine group by year order by avg(rating) desc LIMIT ?1 ", nativeQuery = true)
+    @Query(value="select * from wine group by year order by avg(rating) desc, year LIMIT ?1 ", nativeQuery = true)
     Collection<Wine> findVintageWines(Integer top);
 
 
